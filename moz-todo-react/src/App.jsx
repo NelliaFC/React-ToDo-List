06713dfe0c1389859772f985ebe9b5180eabfa93
NeoNeonilla <nelliafleurova@gmail.com>
2023-12-29 07:44:19 -0600

    First Commit

---
+++ moz-todo-react/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+
+function App(props) {
+ 
+  return (
+    <>
+      <header>
+        <h1>Hello, {props.subject}! </h1>
+        <button type="button" className="primary">Click me!</button>
+      </header>
+    </>
+  );
+}
+
+export default App;
